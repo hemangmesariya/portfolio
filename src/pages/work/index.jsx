8aecbdce331986a0e9d302a10adc3e39d13bd522
unknown <hemangmesariya009@gmail.com>
2022-07-15 22:06:13 +0530

    commit

--- src/pages/work/index.jsx
+++ src/pages/work/index.jsx
@@ -9,16 +9,16 @@ const Work = () => {
       </div>
       <div className="flex flex-col justify-around">
         <div className="grid grid-cols-2">
-          <div className="text-center h-[300px] w-full  mx-auto hover:bg-slate-200">
-            <img src="https://res.cloudinary.com/hemang/image/upload/f_auto/v1657885772/portfolio/project_movie_e1alux.webp" alt="" srcset="" />
+          <div className="text-center h-[400px] w-full overflow-hidden object-scale-down  mx-auto hover:bg-slate-200">
+            <a href="https://google.com" target="_blank"><img className="hover:scale-105 hover:opacity-80 duration-300" src="https://res.cloudinary.com/hemang/image/upload/f_auto/v1657885772/portfolio/project_movie_e1alux.webp" alt="" srcset="" /></a>
           </div>
-          <div className="text-center h-[300px] w-full  mx-auto hover:bg-slate-200">
+          <div className="text-center h-[400px] w-full  mx-auto hover:bg-slate-200">
             work2
           </div>
-          <div className="text-center h-[300px] w-full  mx-auto hover:bg-slate-200">
+          <div className="text-center h-[400px] w-full  mx-auto hover:bg-slate-200">
             work3
           </div>
-          <div className="text-center h-[300px] w-full  mx-auto hover:bg-slate-200">
+          <div className="text-center h-[400px] w-full  mx-auto hover:bg-slate-200">
             work4
           </div>
         </div>
